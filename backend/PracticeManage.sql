b9a6037d8db7078244bd534003244f82d9049b56
Harshvardhan Singh <harshvs1999@gmail.com>
2021-11-11 22:23:23 +0530

    Login API with Database

---
+++ backend/PracticeManage.sql
@@ -0,0 +1,13 @@
+create database Mindbyte;
+use Mindbyte;
+create table user(
+	id int NOT NULL,
+	username varchar (255),
+    password varchar (255)
+);
+
+INSERT INTO user VALUES(1, "Mindbyte", "password");
+INSERT INTO user VALUES(2, "Harsh", "Singh");
+INSERT INTO user VALUES(3, "Rakesh", "R");
+INSERT INTO user VALUES(4, "U", "U");
+select * from user;
